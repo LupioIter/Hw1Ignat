7c1c7d2ccc33fed543175c2a40f1f60629bdb064
Lupio_Оля <skyuii389@gmail.com>
2022-08-19 23:49:23 +0300

    hw5

--- src/p2-homeworks/h5-rrd-v6/HW5.tsx
+++ src/p2-homeworks/h5-rrd-v6/HW5.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import Header from './Header'
 import Pages from './Pages'
+import {HashRouter} from "react-router-dom";
+import s from './HW5.module.css'
 
 function HW5() {
     return (
-        <div>
+        <div className={s.main_container}>
             {/*в gh-pages лучше работает HashRouter*/}
-            {/*<HashRouter>*/}
+            <HashRouter basename={'/'}>
 
             <Header/>
 
             <Pages/>
 
-            {/*</HashRouter>*/}
+            </HashRouter>
         </div>
     )
 }
--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from 'react';
+import {NavLink} from "react-router-dom";
+import {PATH} from "./Pages";
+import s from './Header.module.css';
 
 function Header() {
-    return (
-        <div>
-            // add NavLinks
 
+
+    return (
+        <div className={s.header_wrapper}>
+            <div className={s.sidebar}>
+
+                    <NavLink className={s.link} to={PATH.PRE_JUNIOR}>Pre-junior</NavLink>
+
+                    <NavLink className={s.link} to={PATH.JUNIOR}>Junior</NavLink>
+
+                    <NavLink className={s.link} to={PATH.JUNIOR_PLUS}>Junior-Plus</NavLink>
+
+            </div>
+                <div className={s.hover_div}></div>
         </div>
     )
 }
